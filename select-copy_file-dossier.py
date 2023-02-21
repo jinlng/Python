6e88e552a6e498f58d6e19e303ba6a86d86ee041
jinlng <jinlng@outlook.com>
2023-02-21 16:12:10 +0100

    21/02/2023

---
+++ select-copy_file-dossier.py
@@ -0,0 +1,27 @@
+# Find all files within one mother folder containing a specified keyword 
+# in their names and copy all to a new folder with no overlapping of the same file
+# Liang Jingyi chez Sade Telecom created in 2021
+
+import os
+import shutil
+
+def select_and_copy(folder_org, folder_new, keyword):
+    for dirpath, dirnames, filenames in os.walk(folder_org):
+        for filename in filenames:
+            if keyword in filename:
+                try:
+                    shutil.copy(os.path.join(dirpath, filename), os.path.join(folder_new, filename))
+                except (shutil.SameFileError, PermissionError):
+                    pass
+                except Exception as e:
+                    print(f"Error occurred while copying file: {e}")
+        for dirname in dirnames:
+            select_and_copy(os.path.join(dirpath, dirname), folder_new, keyword)
+
+dir_src = "C:/Users/liang.jingyi/Documents/CALCUL DE CHARGE/PAS/31323"
+dir_new = "C:/Users/liang.jingyi/Documents/test_new"
+keyword = "FicheAppui"
+
+select_and_copy(dir_src, dir_new, keyword)
+
+
