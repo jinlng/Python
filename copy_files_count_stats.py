d5ef522a55a1778c365b85aefd248e0f254bf4e7
jinlng <jinlng@outlook.com>
2023-03-08 18:18:22 +0100

    modification to make the code more concise

--- copy_files_count_stats.py
+++ copy_files_count_stats.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import pandas as pd
 
-folder_org = "C:/Users/liang.jingyi/Documents/CALCUL DE CHARGE/PAS/31327"
+folder_org = "C:/Users/liang.jingyi/Documents/CALCUL DE CHARGE/PDO/37828/D2"
 folder_new = "C:/Users/liang.jingyi/Documents/test_new"
 keyword = "FicheAppui"
 
